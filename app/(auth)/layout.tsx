935c10cc7df86cdd3f61f6174be9b3916b87ca3c
unknown <bdsayaf@gmail.com>
2023-09-01 20:09:47 +0600

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="flex justify-center items-center h-full w-full">{children}</div>;
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+const SetupPage = () => {
+  return <Button size="default">Click Me</Button>;
+};
+
+export default SetupPage;
