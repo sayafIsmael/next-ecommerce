aa095cbeafee92e3ac4d70b7ed67d79ac2687ba9
Abu Sayaf <bdsayaf@gmail.com>
2023-09-03 23:41:40 +0600

    Dashboard page redirect done

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {
+    storeId: string;
+  };
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sing-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>
+        <div>Navbar Area</div>
+        {children}
+      </div>
+    </>
+  );
+}
