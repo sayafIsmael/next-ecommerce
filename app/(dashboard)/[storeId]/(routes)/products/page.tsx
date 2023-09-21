45d2d30abaaa2d8026cfffba72be5664f1bddee0
Abu Sayaf Ismail <45027528+sayafIsmael@users.noreply.github.com>
2023-09-21 15:19:29 +0600

    Product route

---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,32 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { ProductClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
+
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-4 pt-6">
+        <ProductClient data={formattedProducts} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
