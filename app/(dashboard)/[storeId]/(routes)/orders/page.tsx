2a172538752ef26ed98e9e952a4dd1eddf89c16c
Abu Sayaf Ismail <45027528+sayafIsmael@users.noreply.github.com>
2023-09-23 04:15:42 +0600

    Orders module complete

---
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,48 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { OrderClient } from "./components/client";
+import { OrderColumn } from "./components/columns";
+import { currencyFormatter } from "@/lib/utils";
+
+const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
+    id: item.id,
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems.map((item) => item.product.name).join(","),
+    totalPrice: currencyFormatter.format(
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0)
+    ),
+    isPaid: item.isPaid,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-4 pt-6">
+        <OrderClient data={formattedOrders} />
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
